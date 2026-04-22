aa4894c0625518b26ba745a41aaa6670a7581645
jjxlia <bachmayerjulia2004@gmail.com>
2026-04-22 13:51:15 +0200

    Aktuelle Version

---
+++ orders/src/main/java/org/acme/OrderResource.java
@@ -0,0 +1,67 @@
+package org.acme;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import java.util.List;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import java.util.Map;
+
+@Path("/orders")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+
+//@RolesAllowed("user")
+public class OrderResource {
+
+    @GET
+    public List<Order> getAll() {
+        return Order.listAll();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Order getById(@PathParam("id") Long id) {
+        return Order.findById(id);
+    }
+
+    @POST
+    @Transactional
+    public Order bestellen(Order input) {
+
+        Order order = Order.bestellen(
+                input.kundenummer,
+                input.produktnummer,
+                input.menge
+        );
+        order.persist();
+
+        //Camunda Prozess starten
+        Client client = ClientBuilder.newClient();
+
+        String camundaUrl =
+                "http://localhost:8080/engine-rest/process-definition/key/Process_0ltcqh0/start";
+
+        Map<String, Object> body = Map.of(
+                "businessKey", order.id.toString(),
+                "variables", Map.of(
+                        "orderId", Map.of("value", order.id, "type", "Long"),
+                        "kundenummer", Map.of("value", order.kundenummer, "type", "Integer"),
+                        "produktnummer", Map.of("value", order.produktnummer, "type", "Integer"),
+                        "menge", Map.of("value", order.menge, "type", "Integer")
+                )
+        );
+
+        client
+                .target(camundaUrl)
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.json(body));
+
+        client.close();
+
+        return order;
+    }
+}
