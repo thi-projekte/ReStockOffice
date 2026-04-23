d31b9f0021a322f58eccb399d986008691271a42
Authen <merimmuka@gmail.com>
2026-04-23 23:44:54 +0200

    testing

--- orders/src/main/java/org/acme/OrderResource.java
+++ orders/src/main/java/org/acme/OrderResource.java
@@ -14,7 +14,7 @@ import java.util.Map;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 
-//@RolesAllowed("user")
+@RolesAllowed("user") //dem user wird in keycloak console rolle und rechte zugewiesen
 public class OrderResource {
 
     @GET
