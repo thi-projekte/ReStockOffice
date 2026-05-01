d15c601c5130a1b321707668fe49f4850d1d7e5a
pascaldnh <pascal.dinh2003@gmail.com>
2026-05-01 17:41:42 +0200

    Run process engine as REST-only service

--- src/main/java/org/cibseven/getstarted/loanapproval/WebappExampleProcessApplication.java
+++ src/main/java/org/cibseven/getstarted/loanapproval/WebappExampleProcessApplication.java
@@ -16,28 +16,15 @@
  */
 package org.cibseven.getstarted.loanapproval;
 
-import org.cibseven.bpm.engine.RuntimeService;
 import org.cibseven.bpm.spring.boot.starter.annotation.EnableProcessApplication;
-import org.cibseven.bpm.spring.boot.starter.event.PostDeployEvent;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 @EnableProcessApplication
 public class WebappExampleProcessApplication {
 
-  @Autowired
-  private RuntimeService runtimeService;
-
   public static void main(String... args) {
     SpringApplication.run(WebappExampleProcessApplication.class, args);
   }
-
-  @EventListener
-  public void processPostDeploy(PostDeployEvent event) {
-    runtimeService.startProcessInstanceByKey("loanApproval");
-  }
-
 }
