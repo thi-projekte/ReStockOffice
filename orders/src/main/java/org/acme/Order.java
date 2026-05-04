4721dad84263c03b676acfa9074618fa489ad4c3
Authen <merimmuka@gmail.com>
2026-05-05 00:03:48 +0200

    test

---
+++ orders/src/main/java/org/acme/Order.java
@@ -0,0 +1,43 @@
+package org.acme;
+
+import java.time.LocalDateTime;
+import java.util.*;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+//test
+@Entity
+@Table(name = "orders")
+public class Order extends PanacheEntity {
+
+    @NotNull
+    public String username;
+
+    @NotNull
+    public Integer frequency;
+
+    @NotNull
+    public Integer produktnummer;
+
+    @Min(1)
+    public Integer menge;
+
+    public String status = "ERSTELLT";
+    public LocalDateTime createdAt = LocalDateTime.now();
+
+    public static Order bestellen(String username, int produktnummer, int menge, int frequency) {
+        System.out.println("🔥 STATIC bestellen() CALLED");
+        Order bestellung = new Order();
+        System.out.println("🧩 BUILDING ORDER OBJECT");
+        bestellung.username = username;
+        bestellung.produktnummer = produktnummer;
+        bestellung.menge = menge;
+        bestellung.frequency = frequency;
+        bestellung.createdAt = LocalDateTime.now();
+        System.out.println("✅ ORDER READY (NOT SAVED YET)");
+        return bestellung;
+    }
+}
