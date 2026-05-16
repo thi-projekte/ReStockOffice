500a03a88ca553358e2dcb7df9b911e261bf77b2
leVontrex <bastian.miehling04@gmail.com>
2026-05-16 12:39:03 +0200

    🏗️: changed Day to ENUM

--- users/src/main/java/de/restockoffice/DeliveryDay.java
+++ users/src/main/java/de/restockoffice/DeliveryDay.java
@@ -1,11 +1,14 @@
 package de.restockoffice;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
 public enum DeliveryDay {
-    MONTAG,
-    DIENSTAG,
-    MITTWOCH,
-    DONNERSTAG,
-    FREITAG,
-    SAMSTAG,
-    SONNTAG
+    Montag,
+    Dienstag,
+    Mittwoch,
+    Donnerstag,
+    Freitag,
+    Samstag,
+    Sonntag
 }
