6c8e78cb1d296649bce6fdd3652b9e2890d2b813
jjxlia <bachmayerjulia2004@gmail.com>
2026-05-20 22:51:17 +0200

    💄 Transparenz Lieferkosten

--- spa/src/pages/restocker-view/RestockerPage.tsx
+++ spa/src/pages/restocker-view/RestockerPage.tsx
@@ -133,6 +133,7 @@ export function RestockerPage() {
                             <div className="metric-tile">
                                 <div className="metric-label">Heutiger Verdienst</div>
                                 <div className="metric-value">{earningsToday} €</div>
+                                <div className="metric-sub">7€ pro Lieferung</div>
                             </div>
 
                         </div>
