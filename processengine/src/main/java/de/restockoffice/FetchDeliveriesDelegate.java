b0326e19d35bb6f4f2a80b565efd5d6b6a190e48
leVontrex <bastian.miehling04@gmail.com>
2026-05-29 15:25:00 +0200

    💚reverted fix

--- processengine/src/main/java/de/restockoffice/FetchDeliveriesDelegate.java
+++ processengine/src/main/java/de/restockoffice/FetchDeliveriesDelegate.java
@@ -12,16 +12,16 @@ import java.util.Map;
 public class FetchDeliveriesDelegate implements JavaDelegate {
 
     // Anpassen zum eigentlichen Delivery Service !!!
-    /*@Autowired
-    private DeliveryService deliveryService;*/
+    @Autowired
+    private DeliveryService deliveryService;
 
     @Override
     public void execute(DelegateExecution execution) throws Exception {
 
         // Lieferungen für heute holen, gruppiert nach Kunde !!! noch anpassen zum Delivery Service
-        //List<Map<String, Object>> customers = deliveryService.getDeliveriesForToday();
+        List<Map<String, Object>> customers = deliveryService.getDeliveriesForToday();
 
         // Als Prozessvariable speichern – muss eine java.util.List sein!
-        //execution.setVariable("customers", customers);
+        execution.setVariable("customers", customers);
     }
 }
