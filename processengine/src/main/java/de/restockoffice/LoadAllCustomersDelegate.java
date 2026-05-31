36dbf7e492990228de07a1370183f8c296a6c6e9
leVontrex <bastian.miehling04@gmail.com>
2026-05-31 15:50:38 +0200

    🐛fixed: PE authorization

--- processengine/src/main/java/de/restockoffice/LoadAllCustomersDelegate.java
+++ processengine/src/main/java/de/restockoffice/LoadAllCustomersDelegate.java
@@ -2,6 +2,7 @@ package de.restockoffice;
 
 import org.cibseven.bpm.engine.delegate.DelegateExecution;
 import org.cibseven.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
@@ -10,7 +11,11 @@ import java.util.List;
 @Component("loadAllCustomersDelegate")
 public class LoadAllCustomersDelegate implements JavaDelegate {
 
-    private final RestClient userServiceClient = RestClient.create("https://users.restockoffice.de");
+    private final RestClient userServiceClient;
+
+    public LoadAllCustomersDelegate(@Value("${usersservice.base-url}") String usersServiceBaseUrl) {
+        this.userServiceClient = RestClient.create(usersServiceBaseUrl);
+    }
 
     @Override
     public void execute(DelegateExecution execution) {
