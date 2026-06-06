b492f4f3d226b0d0bae2328dc57a6d958a9d201b
Corinna <corinna.niedermeier01@gmail.com>
2026-06-06 12:57:24 +0200

    🔧 Allow abo confirmation process API

---
+++ processengine/src/main/java/de/restockoffice/ProcessEngineApiSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package de.restockoffice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProcessEngineApiSecurityConfiguration {
+
+  @Bean
+  @Order(Ordered.HIGHEST_PRECEDENCE)
+  public SecurityFilterChain aboConfirmationApiSecurityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .securityMatcher("/api/abo-confirmation-process/**")
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+
+    return http.build();
+  }
+}
