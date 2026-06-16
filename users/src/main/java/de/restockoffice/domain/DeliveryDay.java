2c2c1c4d9d20b2a68598eb063a42939ed728e62f
leVontrex <bastian.miehling04@gmail.com>
2026-06-16 10:13:48 +0200

    🐛fix: changed enum for DeliveryDay

--- users/src/main/java/de/restockoffice/domain/DeliveryDay.java
+++ users/src/main/java/de/restockoffice/domain/DeliveryDay.java
@@ -1,15 +1,14 @@
 package de.restockoffice.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonFormat(with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
 public enum DeliveryDay {
-    @JsonProperty("Montag") MONTAG,
-    @JsonProperty("Dienstag") DIENSTAG,
-    @JsonProperty("Mittwoch") MITTWOCH,
-    @JsonProperty("Donnerstag") DONNERSTAG,
-    @JsonProperty("Freitag") FREITAG,
-    @JsonProperty("Samstag") SAMSTAG,
-    @JsonProperty("Sonntag") SONNTAG
+    MONTAG,
+    DIENSTAG,
+    MITTWOCH,
+    DONNERSTAG,
+    FREITAG,
+    SAMSTAG,
+    SONNTAG
 }
