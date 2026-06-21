dfa4aec57a9954d536ca4bfc2d03bf0a7ae0f2e1
leVontrex <bastian.miehling04@gmail.com>
2026-06-21 20:52:41 +0200

    🔒️ fixed sonar Issues

--- users/src/main/java/de/restockoffice/domain/DeliveryDay.java
+++ users/src/main/java/de/restockoffice/domain/DeliveryDay.java
@@ -1,28 +1,15 @@
 package de.restockoffice.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
+@SuppressWarnings("java:S115")
 @JsonFormat(with = JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
 public enum DeliveryDay {
-    @JsonProperty("Montag")
-    MONTAG,
-
-    @JsonProperty("Dienstag")
-    DIENSTAG,
-
-    @JsonProperty("Mittwoch")
-    MITTWOCH,
-
-    @JsonProperty("Donnerstag")
-    DONNERSTAG,
-
-    @JsonProperty("Freitag")
-    FREITAG,
-
-    @JsonProperty("Samstag")
-    SAMSTAG,
-
-    @JsonProperty("Sonntag")
-    SONNTAG
+    Montag,
+    Dienstag,
+    Mittwoch,
+    Donnerstag,
+    Freitag,
+    Samstag,
+    Sonntag
 }
