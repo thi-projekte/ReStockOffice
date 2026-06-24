7d8cab994d5fbe2b30c9e59a20c49977efe203a6
jjxlia <bachmayerjulia2004@gmail.com>
2026-06-24 07:30:45 +0200

    🐛 Keycloak

--- keycloak/theme/restock-office/login/resources/js/logo-link.js
+++ keycloak/theme/restock-office/login/resources/js/logo-link.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const header = document.getElementById('kc-header')
+  var header = document.getElementById('kc-header')
   if (!header) return
   header.style.cursor = 'pointer'
   header.addEventListener('click', function () {
-    globalThis.location.href = 'http://localhost:5173'
+    window.location.href = 'http://localhost:5173'
   })
 })
